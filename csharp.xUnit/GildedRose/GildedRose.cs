77e594ea27e1cd97a1e66ad7c71a8ee219bf9da8
sem-afor <se23m047@technikum-wien.at>
2025-03-26 19:15:40 +0100

    refactoring 1

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -11,79 +11,76 @@ public class GildedRose
         this.Items = Items;
     }
 
+
     public void UpdateQuality()
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
             {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
+                return;
             }
-            else
-            {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
+            Items[i].SellIn -= 1;
+
+            if(Items[i].SellIn > 0)
+            {
+                switch (Items[i].Name)
+                {
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        if (Items[i].SellIn <= 5)
                         {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
+                            Items[i].Quality += 3;
                         }
-
-                        if (Items[i].SellIn < 6)
+                        else if (Items[i].SellIn <= 10)
                         {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
+                            Items[i].Quality += 2;
                         }
-                    }
-                }
-            }
-
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-            {
-                Items[i].SellIn = Items[i].SellIn - 1;
-            }
-
-            if (Items[i].SellIn < 0)
-            {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
+                        else
                         {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
+                            Items[i].Quality += 1;
                         }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
+                        break;
+                    case "Aged Brie":
+                        Items[i].Quality += 1;
+                        break;
+                    case "Conjured":
+                        Items[i].Quality -= 2;
+                        break;
+                    default:
+                        Items[i].Quality -= 1;
+                        break;
                 }
-                else
+            } else
+            {
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        Items[i].Quality = 0;
+                        break;
+                    case "Aged Brie":
+                        Items[i].Quality += 2;
+                        break;
+                    case "Conjured":
+                        Items[i].Quality -= 4;
+                        break;
+                    default:
+                        Items[i].Quality -= 2;
+                        break;
                 }
             }
+
+            if (Items[i].Quality > 50)
+            {
+                Items[i].Quality = 50;
+            }
+
+            if (Items[i].Quality < 0)
+            {
+                Items[i].Quality = 0;
+            }
+
+
         }
     }
 }
