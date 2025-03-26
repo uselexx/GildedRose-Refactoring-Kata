345ccdaa21d9a54f6da9567d3f467cbe87b6860d
Alexander Wedenik <Alexander@Wedenik.com>
2025-03-26 19:33:34 +0100

    implemented level 2

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -2,6 +2,9 @@
 
 namespace GildedRoseKata;
 
+
+
+
 public class GildedRose
 {
     IList<Item> Items;
@@ -14,73 +17,10 @@ public class GildedRose
 
     public void UpdateQuality()
     {
-        for (var i = 0; i < Items.Count; i++)
+        foreach (var item in Items)
         {
-            if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
-            {
-                return;
-            }
-
-            Items[i].SellIn -= 1;
-
-            if(Items[i].SellIn > 0)
-            {
-                switch (Items[i].Name)
-                {
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        if (Items[i].SellIn <= 5)
-                        {
-                            Items[i].Quality += 3;
-                        }
-                        else if (Items[i].SellIn <= 10)
-                        {
-                            Items[i].Quality += 2;
-                        }
-                        else
-                        {
-                            Items[i].Quality += 1;
-                        }
-                        break;
-                    case "Aged Brie":
-                        Items[i].Quality += 1;
-                        break;
-                    case "Conjured":
-                        Items[i].Quality -= 2;
-                        break;
-                    default:
-                        Items[i].Quality -= 1;
-                        break;
-                }
-            } else
-            {
-                switch (Items[i].Name)
-                {
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        Items[i].Quality = 0;
-                        break;
-                    case "Aged Brie":
-                        Items[i].Quality += 2;
-                        break;
-                    case "Conjured":
-                        Items[i].Quality -= 4;
-                        break;
-                    default:
-                        Items[i].Quality -= 2;
-                        break;
-                }
-            }
-
-            if (Items[i].Quality > 50)
-            {
-                Items[i].Quality = 50;
-            }
-
-            if (Items[i].Quality < 0)
-            {
-                Items[i].Quality = 0;
-            }
-
-
+            var strategy = ItemUpdateStrategyFactory.GetStrategy(item);
+            strategy.Update(item);
         }
     }
 }
---
+++ csharp.xUnit/GildedRose/ItemStrategy.cs
@@ -0,0 +1,112 @@
+﻿namespace GildedRoseKata;
+
+public interface IItemUpdateStrategy
+{
+    void Update(Item item);
+}
+
+public static class ItemUpdateStrategyFactory
+{
+    public static IItemUpdateStrategy GetStrategy(Item item)
+    {
+        return item.Name switch
+        {
+            "Aged Brie" => new AgedBrieStrategy(),
+            "Sulfuras, Hand of Ragnaros" => new SulfurasStrategy(),
+            "Backstage passes to a TAFKAL80ETC concert" => new BackstagePassesStrategy(),
+            "Conjured" => new ConjuredItemStrategy(),
+            _ => new NormalItemStrategy(),
+        };
+    }
+
+    
+}
+
+public class NormalItemStrategy : IItemUpdateStrategy
+{
+    public void Update(Item item)
+    {
+        item.SellIn--;
+
+        if (item.Quality > 0)
+        {
+            item.Quality--;
+        }
+
+        if (item.SellIn < 0 && item.Quality > 0)
+        {
+            item.Quality--;
+        }
+    }
+}
+
+public class AgedBrieStrategy : IItemUpdateStrategy
+{
+    public void Update(Item item)
+    {
+        item.SellIn--;
+
+        if (item.Quality < 50)
+        {
+            item.Quality++;
+        }
+
+        if (item.SellIn < 0 && item.Quality < 50)
+        {
+            item.Quality++;
+        }
+    }
+}
+
+public class SulfurasStrategy : IItemUpdateStrategy
+{
+    public void Update(Item item)
+    {
+
+    }
+}
+
+public class BackstagePassesStrategy : IItemUpdateStrategy
+{
+    public void Update(Item item)
+    {
+
+        if (item.Quality < 50)
+        {
+            item.Quality++;
+
+            if (item.SellIn <= 10) item.Quality++;
+            if (item.SellIn <= 5) item.Quality++;
+        }
+
+        item.SellIn--;
+
+        if (item.SellIn < 0)
+        {
+            item.Quality = 0;
+        }
+
+        // Ensure quality stays within bounds
+        if (item.Quality > 50)
+        {
+            item.Quality = 50;
+        }
+    }
+}
+
+public class ConjuredItemStrategy : IItemUpdateStrategy
+{
+    public void Update(Item item)
+    {
+        item.SellIn--;
+
+        if (item.Quality > 0)
+        {
+            item.Quality -= 2;
+        }
+        if (item.SellIn < 0 && item.Quality > 0)
+        {
+            item.Quality -= 2;
+        }
+    }
+}
